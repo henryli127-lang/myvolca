fbf3eae40f692ccf0a5acf705cf398db4e807b46
Henry <henry@HenrydeMacBook-Pro.local>
2026-01-09 17:08:20 +0800

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // 排除 storyspark-ai 文件夹，避免编译旧代码
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules/**', '**/storyspark-ai/**'],
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
 
